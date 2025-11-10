7c7d2ac4b5165cf4782dad774301c0234a0a9c22
Stan-333 <dobriy.che@yandex.ru>
2025-11-10 19:19:57 +0300

    Создание Envelope, использование EndpointResult.

---
+++ DirectoryService/src/Shared/EndpointResults/EndpointResult.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using CSharpFunctionalExtensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Metadata;
+using IResult = Microsoft.AspNetCore.Http.IResult;
+
+namespace Shared.EndpointResults;
+
+public sealed class EndpointResult<TValue> : IResult, IEndpointMetadataProvider
+{
+    private readonly IResult _result;
+
+    public EndpointResult(Result<TValue, Error> result)
+    {
+        _result = result.IsSuccess
+            ? new SuccessResult<TValue>(result.Value)
+            : new ErrorsResult(result.Error);
+    }
+
+    public EndpointResult(Result<TValue, Errors> result)
+    {
+        _result = result.IsSuccess
+            ? new SuccessResult<TValue>(result.Value)
+            : new ErrorsResult(result.Error);
+    }
+
+    public Task ExecuteAsync(HttpContext httpContext) =>
+        _result.ExecuteAsync(httpContext);
+
+    public static implicit operator EndpointResult<TValue>(Result<TValue, Error> result) => new(result);
+
+    public static implicit operator EndpointResult<TValue>(Result<TValue, Errors> result) => new(result);
+
+    public static void PopulateMetadata(MethodInfo method, EndpointBuilder builder)
+    {
+        ArgumentNullException.ThrowIfNull(method);
+        ArgumentNullException.ThrowIfNull(builder);
+
+        builder.Metadata.Add(new ProducesResponseTypeMetadata(500, typeof(Envelope<Errors>), ["application/json"]));
+        builder.Metadata.Add(new ProducesResponseTypeMetadata(400, typeof(Envelope<Errors>), ["application/json"]));
+        builder.Metadata.Add(new ProducesResponseTypeMetadata(404, typeof(Envelope<Errors>), ["application/json"]));
+        builder.Metadata.Add(new ProducesResponseTypeMetadata(409, typeof(Envelope<Errors>), ["application/json"]));
+    }
+}
